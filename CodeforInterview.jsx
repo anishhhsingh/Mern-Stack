5d1c4fefb0491b82b55ec7087f6d2ee889e550ba
gaudy540octopus <93523996+gaudy540octopus@users.noreply.github.com>
2022-10-10 19:40:29 +0530

    Update CodeforInterview.jsx

--- CodeforInterview.jsx
+++ CodeforInterview.jsx
@@ -2,8 +2,8 @@
 
 import { Box, Typography, styled } from '@mui/material';
 
-import Youtube from '../Assets/Images/youtube.png';
-import InstaTele from '../Assets/Images/InstaTele.jpeg';
+import Youtube from 'youtube.png';
+import InstaTele from 'InstaTele.jpeg';
 
 const Header = styled(Box)`
     margin: 50px;
