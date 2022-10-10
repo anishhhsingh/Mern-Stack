9ecf71fcfa678de6d2735953907e613fd9011428
gaudy540octopus <93523996+gaudy540octopus@users.noreply.github.com>
2022-10-10 19:45:46 +0530

    Update route.js

--- route.js
+++ route.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { getUsers, addUser, getUserById, editUser, deleteUser } from '../controller/user-controller.js';
+import { getUsers, addUser, getUserById, editUser, deleteUser } from 'user-controller.js';
 
 const router = express.Router();
 
